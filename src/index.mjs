3a04a6a17f7b753bbb58611297d496f85070c702
tomooyaito <107048356+TOMOYAITO0726@users.noreply.github.com>
2023-10-25 22:10:06 +0000

    基礎文法変数宣言

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,23 @@
-import "./styles.css";
+/**
+ * const,let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var val1 = "var変数";
+console.log(val1);
+//var変数は上書き可能
+val1="var変数を上書き";
+console.log(val1);
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//     name: "いとと",
+//     age: 28,
+// };
+// val4.name ="it"
+// val4.address ="Chiba";
+// console.log(val4);
+// constで定義した配列はプロパティの変更が可能
+
+const val5 =['dog', 'cat'];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
