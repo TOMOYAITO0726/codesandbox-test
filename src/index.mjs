44af9c3f8043cdfdde3bbfba77f2defc4a103a14
tomooyaito <107048356+TOMOYAITO0726@users.noreply.github.com>
2023-10-27 23:02:33 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -113,11 +113,50 @@
 /**
  * mapやfilterを使った配列の処理
  */
-const nameArr =["田中","山田","佐藤"];
+//const nameArr =["田中","山田","佐藤"];
 //returnされた結果にに基づいて新しい配列を生成する
 // const nameArr2 = nameArr.map((name)=>{
 //     return name;
 // })
 // console.log(nameArr2);
 //配列をループして処理する
-///nameArr.map((name) => console.log(name));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+//Filter→ある条件に一致した値を取り出す
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//     return num % 2 == 0;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//     if (name == "田中"){
+//         return name
+//     } else {
+//         return `${name}さん`
+//     }
+// })
+// console.log(newNameArr);
+/**
+ * 三項演算子
+ */
+// const num = 1300;
+//console.log(num.toLocaleString()); //カンマ区切りで表示
+// const formattedNum = typeof num == `number` ? num.toLocaleString() : `数値を入力してください`;
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//     return num1 + num2 > 100 ? `100を超えています!!` : `許容範囲内です`;
+// }
+// console.log(checkSum(20, 81));
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// ||は左側がflaseなら右を返す 左側がtrueなら左側を返す
+// const num = null;
+// const fee = num || "金額未設定です"
+// console.log(fee);
+//　&&は左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
